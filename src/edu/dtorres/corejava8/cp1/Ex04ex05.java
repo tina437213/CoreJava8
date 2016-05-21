ce545cbd3f03048c0bd9590f62ded8cd68982baa
diego-torres <diego.torres.fuerte@gmail.com>
2016-05-21 19:07:32 -0400

    Exercise 1.4 and 1.5

---
+++ src/edu/dtorres/corejava8/cp1/Ex04ex05.java
@@ -0,0 +1,20 @@
+package edu.dtorres.corejava8.cp1;
+
+/**
+ * 
+ * Write a program that prints the smallest and largest positive double value.
+ * Hint: Look up Math.nextUp in the Java API
+ * 
+ * @author diego
+ *
+ */
+public class Ex04ex05 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(String.format("Min double: %e", Double.MIN_VALUE));
+		System.out.println(String.format("Max double: %e", Double.MAX_VALUE));
+		int i = (int) Double.MAX_VALUE;
+		System.out.println(String.format("Casted int: %d, maxInt: %d", i, Integer.MAX_VALUE));
+		// Observation: Information lost.
+	}
+}
