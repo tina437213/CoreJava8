7169d9ae193e45c52170d70f1330a2c5778cb29f
diego-torres <diego.torres.fuerte@gmail.com>
2016-05-21 19:05:58 -0400

    Exercise 1.1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CoreJava8
 core java for the impatient by Cay S Horstmann
+
+This project includes my solutions to the exercises of the book. If anyone finds more solutions to them or have any comments or suggestions, please don't hesitate to git about it.
---
+++ src/edu/dtorres/corejava8/cp1/Ex01.java
@@ -0,0 +1,36 @@
+package edu.dtorres.corejava8.cp1;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * Write a program that reads an integer and prints it in binary, octal and
+ * hexadecimal. Print the reciprocal as hexadecimal floating-pint number.
+ * 
+ * @author diego
+ *
+ */
+public class Ex01 {
+
+	public static void main(String... args) {
+		System.out.print("Please provide an integer: ");
+		Scanner in = new Scanner(System.in);
+		Integer i = in.nextInt();
+		in.close();
+		// Calculate reciprocal.
+		// + "Hex Float Input: %a \n"
+		// Reciprocal = 1/integer
+		float reciprocal = 1/i;
+		
+		String unformattedMsg = "Decimal Input: %d \n" 
+				+ "Binary: %s \n"
+				+ "Octal: %o \n" 
+				+ "Hex: %X \n" 
+				+ "Hex Reciprocal: %a \n"
+				+ "END";
+		String outputMsg = String.format(unformattedMsg, i, Integer.toBinaryString(i), i, i, reciprocal);
+
+		System.out.println(outputMsg);
+
+	}
+}
