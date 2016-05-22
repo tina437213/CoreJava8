00067078752e4203db880dee80b0669a7a3402bb
diego-torres <diego.torres.fuerte@gmail.com>
2016-05-22 18:40:44 -0400

    Exercise 1.16

--- src/edu/dtorres/corejava8/cp1/Ex16.java
+++ src/edu/dtorres/corejava8/cp1/Ex16.java
@@ -10,7 +10,14 @@ public class Ex16 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(average(3, 4, 5, 20));
+	}
 
+	public static double average(double value1, double... values) {
+		double sum = value1;
+		for (double v : values)
+			sum += v;
+		return sum / (values.length + 1);
 	}
 
 }
