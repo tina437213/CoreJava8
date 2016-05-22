3f3447b8ccd7fb0841eaa8643b9b1be10eae5e87
diego-torres <diego.torres.fuerte@gmail.com>
2016-05-22 18:33:51 -0400

    Exercise 1.15

--- src/edu/dtorres/corejava8/cp1/Ex15.java
+++ src/edu/dtorres/corejava8/cp1/Ex15.java
@@ -1,5 +1,8 @@
 package edu.dtorres.corejava8.cp1;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Write a program that stores Pascal's triangle up to a given n in an
  * ArrayList<ArrayList<Integer>>.
@@ -9,6 +12,29 @@ package edu.dtorres.corejava8.cp1;
  */
 public class Ex15 {
 	public static void main(String[] args) {
-		System.out.println("NOT IMPLEMENTED YET");
+		System.out.println("***** PASCAL TRIANGLE WITH ArrayLists *****");
+		System.out.print("How many rows?:");
+		Scanner scanner = new Scanner(System.in);
+		int length = scanner.nextInt();
+		scanner.close();
+		
+		// Build the triangle
+		ArrayList<ArrayList<Integer>> pascal = new ArrayList<>();
+		for(int i = 0; i < length; i++){
+			pascal.add(new ArrayList<Integer>());
+			pascal.get(i).add(1);
+			for(int j = 1; j < i; j++){
+				pascal.get(i).add(pascal.get(i-1).get(j-1) + pascal.get(i-1).get(j));
+			}
+			pascal.get(i).add(1);
+		}
+		
+		// Print the triangle
+		for(ArrayList<Integer> row : pascal){
+			for(Integer element : row){
+				System.out.printf("%4d", element);
+			}
+			System.out.println();
+		}
 	}
 }
